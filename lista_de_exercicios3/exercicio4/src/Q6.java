fa02af2f9fff71586e3bf299afa6a4cf41639afe
Bruno Rodrigues Souza <bruno.rodriguesouza1@gmail.com>
2020-09-01 17:28:01 -0300

    Fazendo Exercício 4 da Lista de Exercícios 3

---
+++ lista_de_exercicios3/exercicio4/src/Q6.java
@@ -0,0 +1,31 @@
+/*
+ * Classname             (StaticTeste)
+ *
+ * Date                  (01/09/2020 - 17:27)
+ *
+ * author                (Bruno Rodrigues Souza)
+ *
+ * Copyright notice      (Lista de Exercícios 3 - Exercício 4)
+ */
+public class Q6 {
+    public static void main (String args[]) {
+        Holder h = new Holder();
+        h.held = 100;
+        h.bump(h);
+
+        System.out.println(h.held);
+    }
+
+    class Holder {
+        public int held;
+        public void bump(Holder theHolder) {
+            theHolder.held++;
+        }
+    }
+}
+
+/*
+* Questão 3 - Qual o valor que será printado na linha 7?
+* Resposta - O valor printado será 101.
+*
+*/
