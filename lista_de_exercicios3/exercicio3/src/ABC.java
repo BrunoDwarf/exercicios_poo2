607badb5ffa3e9187ae8dd4ec540a5d17b195898
Bruno Rodrigues Souza <bruno.rodriguesouza1@gmail.com>
2020-09-01 17:05:29 -0300

    Fazendo Exercício 3 da Lista de Exercícios 3.

---
+++ lista_de_exercicios3/exercicio3/src/ABC.java
@@ -0,0 +1,27 @@
+/*
+ * Classname             (ABC)
+ *
+ * Date                  (01/09/2020 - 16:54)
+ *
+ * author                (Bruno Rodrigues Souza)
+ *
+ * Copyright notice      (Lista de Exercícios 3 - Exercício 3)
+ */
+
+public class ABC {
+    public static void main (String args[]) {
+        int x = 10;
+        String s;
+
+        if (x == 10) {
+            s = new String ("cinco");
+            System.out.println(s);
+        }
+    }
+}
+
+/*
+* Questão 3 - O programa a seguir compila com erros (sim, não ou por que não?).
+* Resposta - Não compila pois s é uma variavel local e precisa ser inicializada.
+*
+*/
