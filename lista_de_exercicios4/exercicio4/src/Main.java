5a7e59d7461a7270b4aacce25fbb65af7020568f
Bruno Rodrigues Souza <bruno.rodriguesouza1@gmail.com>
2020-09-11 17:34:29 -0300

    Fazendo o Exercício 4 da Lista de Exercícios 4 parte 1.

---
+++ lista_de_exercicios4/exercicio4/src/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Classname             (Main)
+ *
+ * Date                  (11/09/2020 - 15:35)
+ *
+ * author                (Bruno Rodrigues Souza)
+ *
+ * Copyright notice      (Lista de Exercícios 4 - Exercício 3)
+ */
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+
+    }
+}
