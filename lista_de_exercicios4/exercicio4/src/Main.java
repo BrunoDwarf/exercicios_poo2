fd1a1ecb636d5926046f0e28e0ddf3c19516194f
Bruno Rodrigues Souza <bruno.rodriguesouza1@gmail.com>
2020-09-17 21:35:25 -0300

    Fazendo Exercício 4 da Lista de Exercício 4 parte final.

--- lista_de_exercicios4/exercicio4/src/Main.java
+++ lista_de_exercicios4/exercicio4/src/Main.java
@@ -5,7 +5,7 @@
  *
  * author                (Bruno Rodrigues Souza)
  *
- * Copyright notice      (Lista de Exercícios 4 - Exercício 3)
+ * Copyright notice      (Lista de Exercícios 4 - Exercício 4)
  */
 import java.util.*;
 
@@ -13,6 +13,56 @@ public class Main {
     public static void main(String args[]) {
         Scanner ler = new Scanner(System.in);
 
+        int quantidade_numero, i, z, auxiliar_vetor, auxiliar_ordena, ordena_numero;
 
+        try
+        {
+            System.out.println("Qual a quantidade de números inteiros que será informado: ");
+            quantidade_numero = ler.nextInt();
+
+            //criando vetor com a quantidade de posições que o usuário deseja
+            int vetorNumero[] = new int[quantidade_numero];
+
+            //preenchendo vetor
+            System.out.println("\nInformando números inteiros:");
+            for (i = 0; i < quantidade_numero; i++) {
+                z = i + 1;
+
+                System.out.println("Digite o " + z + "° número: ");
+                vetorNumero[i] = ler.nextInt();
+            }
+
+            //ordenando números do vetor
+            auxiliar_ordena = quantidade_numero - 1;
+            while (auxiliar_ordena > 0)
+            {
+                ordena_numero = 0;
+                for (i = 0; i < auxiliar_ordena; i++) {
+                    if (vetorNumero[i] > vetorNumero[i+1])
+                    {
+                        auxiliar_vetor = vetorNumero[i];
+                        vetorNumero[i] = vetorNumero[i+1];
+                        vetorNumero[i+1] = auxiliar_vetor;
+
+                        ordena_numero = i;
+                    }
+                }
+                auxiliar_ordena = ordena_numero;
+            }
+
+            System.out.println("Listando números informados ordenados: ");
+            for (i = 0; i < quantidade_numero; i++) {
+                System.out.println(vetorNumero[i]);
+            }
+
+        }
+        catch(InputMismatchException e)
+        {
+            System.out.println("Valor digitado não corresponde ao solicitado!");
+        }
     }
 }
+/*
+* Questão 4 - Faça um programa que ordene um conjunto de números inteiros fornecidos pelo usuário por
+* linha de comando e os exiba de modo ordenado um por linha.
+*/
